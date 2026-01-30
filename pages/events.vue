da9c282dd8cfe76753abb392b7f34c907f7b73ad
Lorenzo Marranini <marraninilorenzo@gmail.com>
2026-01-30 13:37:38 +0100

    modifica

--- pages/events.vue
+++ pages/events.vue
@@ -1,11 +1,5 @@
 <template lang="">
     <Main>
-        <div class="pt-32 min-h-screen bg-gray-50">
-            <div class="container mx-auto px-4 mb-8 text-center">
-                <h1 class="text-4xl font-bold text-primary">I Nostri Tornei</h1>
-                    <p class="text-gray-600 mt-2">Scopri i prossimi appuntamenti in programma</p>
-            </div>
-        </div>
         <UpcomingEventSection />
     </Main>
 </template>
